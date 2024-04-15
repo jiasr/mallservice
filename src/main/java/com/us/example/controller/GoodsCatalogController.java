fb59a4b1532b3abce5209ce4b2f51d9582f6f985
jiasr <jiasirui3@163.com>
2024-04-16 00:07:51 +0800

    解决浏览器跨域问题

--- src/main/java/com/us/example/controller/GoodsCatalogController.java
+++ src/main/java/com/us/example/controller/GoodsCatalogController.java
@@ -12,10 +12,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
@@ -24,6 +21,7 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
+@CrossOrigin
 @RequestMapping(value = "/goodscatalog")
 public class GoodsCatalogController {
 
--- src/main/java/com/us/example/controller/UserController.java
+++ src/main/java/com/us/example/controller/UserController.java
@@ -19,6 +19,7 @@ import net.sf.json.JSONObject;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -28,6 +29,7 @@ import com.us.example.util.CommonUtil;
 
 
 @Controller
+@CrossOrigin
 @RequestMapping(value = "/users")
 public class UserController {
     private static final Log logger = LogFactory.getLog(UserController.class);
